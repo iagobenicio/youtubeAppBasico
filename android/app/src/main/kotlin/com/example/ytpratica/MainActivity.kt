999c2fd0e75178569129b90ae35ca6c723ef277e
Iago Benicio <iagobensen@gmail.com>
2021-07-20 17:22:27 -0300

    inicio

---
+++ android/app/src/main/kotlin/com/example/ytpratica/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ytpratica
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/inscricao.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Inscricao extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: Center(
+        child: Text("inscrições"),
+      ),
+    );
+  }
+}
